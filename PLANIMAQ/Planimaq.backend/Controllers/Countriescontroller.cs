40c7a4abbe2320502fbd5fd51bf0233bbce26815
Alfredo Sotero Murga <alfredo.sotero@fursysperu.com.pe>
2025-12-15 12:37:33 -0500

    add post and get for countries

---
+++ PLANIMAQ/Planimaq.backend/Controllers/Countriescontroller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Planimaq.backend.Data;
+using Planimaq.Shared.Entities;
+using System.Threading.Tasks;
+
+namespace Planimaq.backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+
+            //var result = await _context.Countries.ToListAsync();
+            //return Ok(result);
+            return Ok(await _context.Countries.ToListAsync());
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country) { 
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+
+        }
+
+    }
+}
--- PLANIMAQ/Planimaq.backend/Program.cs
+++ PLANIMAQ/Planimaq.backend/Program.cs
@@ -1,15 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Planimaq.backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(
+    opt => {
+        opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("LocalConnection")
+        );
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
